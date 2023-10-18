20a53646401f88cf3a68e33b0a2bb3064e3091fd
hvannieuwenh <helena.vannieuwenhuizen@stonybrook.edu>
2023-10-18 15:22:18 -0400

    Removed unnecessary php

--- experiment.js
+++ experiment.js
@@ -59,14 +59,7 @@ function exemplar_stimuli(stim_path, pack_ID, category, indices, block){
     );
 }
 
-function saveData(name, data){
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST', 'write_data.php'); // 'write_data.php' is the path to the php file described above.
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.send(JSON.stringify({filedata: data}));
-}
-
-const DEBUG_MODE = false
+const DEBUG_MODE = true
 
 var jsPsych = initJsPsych({
     on_finish: function() {
